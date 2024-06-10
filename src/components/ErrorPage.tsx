83f282d0b46aa17afed824375467d490d77bb4ac
Wojciech Balda <wojtek.balda@gmail.com>
2024-06-10 13:29:13 +0200

    Create ErrorPage component

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import ContentContainer from "./ContentContainer";
+import H1 from "./H1";
+
+export default function ErrorPage()
+{
+    return <ContentContainer>
+        <H1>Error. Something went wrong</H1>
+    </ContentContainer>
+}
